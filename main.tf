ecf7043798f0b7d5d3fdede1e07b7b416236b4db
ottagit <ottaosawa@gmail.com>
2023-09-19 19:12:30 +0300

    deploy a single web server

--- main.tf
+++ main.tf
@@ -5,8 +5,28 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, world!" > inde.html
+              nohup busybox httpd -f -p 8080 &
+  EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "dev-server"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "dev-server-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
