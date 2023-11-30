ab0604d319110b0b343d329853f99404476815ac
ottagit <ottaosawa@gmail.com>
2023-11-30 22:44:36 +0300

    restore remote backend to Amazon S3

--- main.tf
+++ main.tf
@@ -39,6 +39,11 @@ resource "aws_security_group_rule" "allow_testing_inbound" {
 terraform {
   backend "s3" {
     key = "stage/services/webserver-cluster/terraform.tfstate"
+    bucket = "batoto-bitange"
+    region = "us-east-1"
+
+    dynamodb_table = "terraone-locks"
+    encrypt = true
   }
 }
 
